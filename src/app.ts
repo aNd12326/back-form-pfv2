63b903562c7fceb5798b4b402adc1c5d70375ae1
Anderson VTWNDS <andersonventosilla0@gmail.com>
2025-04-07 19:43:55 -0500

    cors added

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import formRoutes from './routes/formRoutes';
 import { errorHandler } from './middlewares/errorHandler';
 import mongoose from 'mongoose';
 import config from './config/config';
+import cors from 'cors'
 
 mongoose.connect(config.mongoUri).then(() => {
     console.log('Conectado a MongoDB');
@@ -12,6 +13,16 @@ mongoose.connect(config.mongoUri).then(() => {
 })
 
 const app = express();
+
+// Configura CORS
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE',  // Métodos permitidos
+    allowHeaders: 'Content-Type, Authorization', // Cabeceras permitidas
+    credentials: true, // Permitir credenciales (cookies, autorización, etc.)
+}
+
+app.use(cors(corsOptions));
 
 // Middleware para parsear JSON
 app.use(express.json());
